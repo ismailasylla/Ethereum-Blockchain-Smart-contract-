dcd1543c38fe5911ec4259d6454a417449ff4188
ismaila sylla <ismailasylla@ismailas-MacBook-Pro.local>
2020-10-14 14:10:04 +0400

    Switched infura api provider to kovan testnet for deployment

--- deploy.js
+++ deploy.js
@@ -1,10 +1,23 @@
-const HDWalletProvider = require('truffle-hdwallet=provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
 
 const provider = new HDWalletProvider(
     'process write wage donate clutch barely oxygen sock olympic word buffalo warrior',
-    'https://rinkeby.infura.io/v3/2406b3c192a0410cad2db3f5c92b6140'
+    'https://kovan.infura.io/v3/2406b3c192a0410cad2db3f5c92b6140'
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () =>{
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments:['Hi there!']}).send({gas:'1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+
+};
+
+deploy();
